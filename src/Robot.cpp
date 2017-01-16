4647c5fd0c602be369fe466236b197243487a2e5
Elijah <elijahmtcastle@gmail.com>
2017-01-16 17:28:39 -0500

    Code o' the day
    updated code with vision commented out

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
 #include <memory>
 #include <string>
-
+#include <thread>
 #include <WPILib.h>
-
+#include <CameraServer.h>
 #include <IterativeRobot.h>
-
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/types.hpp>//all of these might not be neccisary
 #include <LiveWindow/LiveWindow.h>
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
 
-class Robot: public frc::IterativeRobot {
+class Robot: public frc::IterativeRobot {//uncoment to enable vision
+	/*static void VisionThread(){// multithreading is required for the image proccessing so yah
+			 cs::UsbCamera camera = CameraServer::GetInstance()->StartAutomaticCapture();//starts capturing basic images into camera
+
+			 camera.SetExposureAuto();
+			 camera.SetResolution(640,480);
+
+			 cs::CvSink sinker = CameraServer::GetInstance()->GetVideo();//Grabs video to sink into the mat image cruncher
+
+			 cs::CvSource cheese =CameraServer::GetInstance()->PutVideo("Rectangle",640,480);//Serves up the images gathered your on camera
+
+			 cv::Mat cruncher;//this is the magic image cruncher right here it eats up the memory to so don't make any more
+
+		 while(true){//image processing happens in here
+
+				 if(sinker.GrabFrame(cruncher)==0){// if theres nothing there you got problems
+
+					 cheese.NotifyError(sinker.GetError());//HEY LISTEN! you got some problems tell me about them
+					 continue;//restarts the thread I think
+			 }
+				 rectangle(cruncher, cv::Point(100, 100), cv::Point(400, 400),cv::Scalar(255, 255, 255), 5);//draw some rectangles on that thing WOOT RECTANGLES
+
+				 			cheese.PutFrame(cruncher);//finally put the final modified frame
+
+		 }
+	}*/
 public:
+
 	double Leftgo,Rightgo;
 
 
@@ -21,13 +49,19 @@ public:
 
 	Spark *fLeft =new Spark(0);
 	Spark *fRight =new Spark(1);
-	Spark *bLeft =new Spark(2);
-	Spark *bRight =new Spark(3);
+	Talon *bLeft =new Talon(2);
+	Talon *bRight =new Talon(3);
+
+	Encoder *encRight=new Encoder(0,1);
+	Encoder *encLeft=new Encoder(2,3);
 
 	RobotDrive *robotDrive =new RobotDrive(fLeft,fRight,bLeft,bRight);
 
 	void RobotInit() {
-		chooser.AddDefault(autoNameDefault, autoNameDefault);
+		//std::thread camthread(VisionThread);//makes a new thread
+		//camthread.detach();//snaps the thread off to do its own thing
+
+		chooser.AddDefault(autoNameDefault, autoNameDefault);//I don't like this look into making it logical
 		chooser.AddObject(autoNameCustom, autoNameCustom);
 		frc::SmartDashboard::PutData("Auto Modes", &chooser);
 
@@ -67,6 +101,8 @@ public:
 	void TeleopInit() {
 		Leftgo=0;
 		Rightgo=0;
+		encRight->Reset();
+			encLeft->Reset();
 
 	}
 
@@ -75,6 +111,10 @@ public:
 		Rightgo=.75*rightDrive->GetRawAxis(1);
 
 		robotDrive->TankDrive(Leftgo,Rightgo);
+
+
+		SmartDashboard::PutNumber("encRight",encRight->GetRaw());
+		SmartDashboard::PutNumber("encLeft",encLeft->GetRaw());
 
 		SmartDashboard::PutNumber("Leftgo",Leftgo);
 		SmartDashboard::PutNumber("Rightgo",Rightgo);
