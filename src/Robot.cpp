a8c1ecd06f6a8b7cac01365178913d8898b26156
Elijah <elijahmtcastle@gmail.com>
2018-02-09 19:00:43 -0500

    Update Robot.cpp
    did some stuff pnumatic arm cleanup.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -10,19 +10,27 @@
 #include <opencv2/core/core.hpp>
 #include <Encoder.h>
 
-
 class Robot: public frc::IterativeRobot {
+private:
+	frc::SendableChooser<std::string> chooser;
+	 std::string autoForward= "Forward";
+	const std::string autoNormal = "Let The Magic Happen";//do some Code Magic! YAH!
+	const std::string autoNone = "NONE!";
+	std::string autoSelected;
 public:
 	float lDrive,rDrive;
 	float climby;
+	int lDis,rDis;
+	bool armout,armin;
 
 	Spark *fLeft =new Spark(0);
 	Spark *bLeft =new Spark(1);
 	Spark *fRight =new Spark(2);
 	Spark *bRight =new Spark(3);
-	Spark *gantry =new Spark(4);
+	Spark *elevator =new Spark(4);
 
-	frc::Encoder *flimflam =new Encoder(0,1);
+	frc::Encoder *encLeft =new Encoder(0,1);
+	frc::Encoder *encRight =new Encoder(2,3);
 	cs::UsbCamera cam= CameraServer::GetInstance()->StartAutomaticCapture();
 
 	Joystick *leftStick =new Joystick(0);
@@ -32,17 +40,20 @@ public:
 	frc::Compressor *garry= new Compressor(0);
 	frc::DoubleSolenoid *arm =new DoubleSolenoid(0,1);
 
+	std::string gameData;
 
 	frc::RobotDrive *robotDrive =new frc::RobotDrive (fLeft,bLeft,fRight,bRight);
 
 	void RobotInit() {
 
 		arm->Set(frc::DoubleSolenoid::kReverse);
-
-
 		cam.SetBrightness(1200);
 		cam.SetExposureManual(42);
 		cam.SetWhiteBalanceManual(3800);
+		chooser.AddDefault(autoNormal,autoNormal);
+		chooser.AddObject(autoForward, autoForward);
+		chooser.AddObject(autoNone, autoNone);
+		frc::SmartDashboard::PutData("Auto Modes", &chooser);
 
 	}
 
@@ -58,9 +69,36 @@ public:
 	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
 
-	void AutonomousInit() override {
+	void AutonomousInit() override{
+		gameData = frc::DriverStation::GetInstance().GetGameSpecificMessage();
+
+		autoSelected = chooser.GetSelected();
+		std::cout << "Auto selected: " << autoSelected << std::endl;
+
+		encLeft->Reset();
+		encRight->Reset();
 
 	}
+
+	void AutonomousPeriodic() {
+		lDis=encLeft->GetRaw();
+		rDis=encRight->GetRaw();
+		//Forward
+		if(autoSelected==autoNormal){
+			if(lDis<=2011&&rDis<=2011){
+				lDrive=.7;
+				rDrive=.7;
+			}
+			else{
+				lDrive=.7;
+				rDrive=.7;
+			}
+		}
+		//Forward
+
+		robotDrive->TankDrive(lDrive,rDrive);
+	}
+
 	//AUTO END
 
 
@@ -70,7 +108,6 @@ public:
 	}
 	//TELE START
 	void TeleopPeriodic() {
-
 		lDrive=.7*leftStick->GetRawAxis(1);
 		rDrive=.7*rightStick->GetRawAxis(1);
 		robotDrive->TankDrive(lDrive,rDrive);
@@ -79,15 +116,16 @@ public:
 		if (fabs(climby) < .1) {
 			climby = 0;
 		}
-		gantry->Set(climby);
-
-		if(gamePad->GetRawButton(3)){
+		elevator->Set(climby);
+		armout=gamePad->GetRawButton(3);
+		armin=gamePad->GetRawButton(4);
+		if(armout){
 			arm->Set(frc::DoubleSolenoid::kReverse);
 		}
-		else if(gamePad->GetRawButton(4)){
+		else if(armin){
 			arm->Set(frc::DoubleSolenoid::kForward);
 		}
-
+		frc::SmartDashboard::PutData("Auto Modes", &chooser);
 		SmartDashboard::PutNumber("Output", climby );
 		SmartDashboard::PutNumber("Raw", gamePad->GetRawAxis(1));
 
@@ -105,7 +143,7 @@ START_ROBOT_CLASS(Robot)
  *		1 Back	"
  *		2 Front	Right
  *		3 Back	"
- *		4 Gantry
+ *		4 Elevator
  *		5
  *		6
  *		7
